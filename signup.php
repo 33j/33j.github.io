929b58d2ecdef4c778e6170367e9a8716ba2c566
cheng306 <cheng306@uw.edu>
2016-08-13 15:20:40 -0700

    a lot of changes

--- signup.php
+++ signup.php
@@ -1,4 +1,9 @@
-<DOCTYPE! html>
+<?php
+	if (isset($_COOKIE["name"])){
+		header("location: index.html");
+	}
+?>
+
 <html>
 	<head>
 		<link type="text/css" rel="stylesheet" href="signupstyle.css"/>
@@ -7,6 +12,54 @@
 	</head>
 	
 	<body>
+
+
+	<?php
+		
+		// first set the name to be error
+		$userNameErr = $firstNameErr = $lastNameErr = $contactErr = $passwordErr = "";
+		$userName = $firstName = $lastName = $address = $city=$contact = $password = $confpass = "";
+		$valid = TRUE;
+		$until = FALSE;
+		if ($_SERVER['REQUEST_METHOD']=="POST"){
+			if (empty($userName=$_POST["username"])){
+				$userNameErr = "UserName is required";
+				$valid = FALSE;
+			}
+			else if (!preg_match("/^[a-zA-Z0-9]*$/",$userName)){
+				$userNameErr = "Only numbers and letters in UserName";
+				$valid = FALSE;
+			}
+			if (empty($firstName=$_POST["firstname"])){
+				$firstNameErr = "First Name is required";
+				$valid = FALSE;
+			}
+			else if (!preg_match("/^[a-zA-Z]*$/",$firstName)){
+				$firstNameErr = "Only letters in First Name";
+				$valid = FALSE;
+			}
+			if (empty($lastName=$_POST["lastname"])){
+				$lastNameErr = "Last Name is required";
+				$valid = FALSE;
+			}
+			else if (!preg_match("/^[a-zA-Z]*$/",$lastName)){
+				$lastNameErr = "Only letters in Last Name";
+				$valid = FALSE;
+			}
+
+
+			
+ 		$until = TRUE;
+		} 
+		if ($valid&&$until){
+			echo "<script type='text/javascript'>window.location.href = 'http://yahoo.com';</script>";
+        	//exit();
+		}
+	?>
+
+
+	
+
 	
 		<ul>
 			<li class="menu"><a class="active" href="index.html">Home</a></li>
@@ -14,29 +67,33 @@
 			<li class="menu"><a href="#about">About</a></li>
 		</ul>
 		
-		<ol type = "1" style="list-style-type:none">
-			<form action="$_SERVER['PHP_SELF']">
-			
+		
+		<form action="" method="post">
+			<ol type = "1" style="list-style-type:none">
+					
 				<div>
 					<div class="left"><li>UserName:</li></div>
-					<div class="right"><input class="text" type="text" name="username"></div>
+					<div class="right"><input class="text" type="text" name="username" value="<?php print $userName;?>">
+						<span> * <?php print $userNameErr ?></span></div>
 				</div>
 				
 				<div>
 					<div class="left"><li>First Name:</li></div>
-					<div class="right"><input class="text" type="text" name="firstname"></div>
+					<div class="right"><input class="text" type="text" name="firstname" value="<?php print $firstName;?>">
+						<span> *  <?php //print $firstNameErr ?> </span></div>
 				</div>
 				
 				<div>
 					<div class="left"><li>Last Name:</li></div>
-					<div class="right"><input class="text" type="text" name="lastname"></div>
+					<div class="right"><input class="text" type="text" name="lastname" value="<?php print $lastName;?>">
+						<span> * <?php //print $lastNameErr ?></span></div>
 				</div>
 				
-			</form>
 			
 			
 			
-			<form>
+			
+		
 				<div>
 					<div class="left"><li>Address:</li></div>
 					<div class="right"><input class="text" type="text" name="address"></div>
@@ -51,9 +108,8 @@
 					<div class="left"><li>State:</li></div>
 					<div class="right"><input class="text" type="text" name="state"></div>
 				</div>
-			</form>
+	
 
-			<form>
 		
 				<div>
 					<div class="left"><li>e-mail:</li></div>
@@ -62,30 +118,40 @@
 				
 				<div>
 					<div class="left"><li>Conact number:</li></div>
-					<div class="right"><input class="text" type="text" name="phone"></div>
+					<div class="right"><input class="text" type="text" name="phone">
+						<span> * <?php print $contactErr ?></span></div>
 				</div>
 				
-			</form>
 
-			<form>
 				<div>
 					<div class="left"><li>Password:</li></div>
-					<div class="right"><input class="text" type="text" name="password"></div>
+					<div class="right"><input class="text" type="text" name="password">
+						<span> * <?php print $passwordErr ?></span></div>
 				</div>
 				
 				<div>
 					<div class="left"><li>Confirm Password:</li></div>
-					<div class="right"><input class="text" type="text" name="confpass"></div>
+					<div class="right"><input class="text" type="text" name="confpass"><span> *</span></div>
 				</div>
 				
-			</form>
+			
 			</ol>
 
-		<div>
-			<div class="left"><input class="finish" type="submit" value="Finish" style="text-align:center;"></div>
-			<div class="right"><a href="login.html"><input type="submit" value="Cancel"></a></div>
-		</div>
+			<div>
+				<div class="left"><input class="finish" type="submit" value="Finish" style="text-align:center;"></div>
+				<div class="right"><a href="login.html"><input type="reset" value="Cancel"></a></div>
+			</div>
+		</form>
+	<div>
+		<?php 
+			//var_dump($valid);
+			//print "<br>$userName" ; 
+		?>
 
-	</body>
+		
+	</div>
+
+
+	</body>	
 
 </html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+	echo "1<br>";
+	exit();
+	echo "2<br>";
+?>
+
+<?php
+	echo "3<br>";
+	exit();
+	echo "4<br>";
+?>
