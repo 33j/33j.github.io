ccd0b6e7774a4cb65befc9500447561f33fd04d8
cheng306 <cheng306@uw.edu>
2016-08-20 16:32:26 -0700

    a lot of changes

--- BuyersPage.php
+++ BuyersPage.php
@@ -1,40 +1,90 @@
 <?php
-	session_start();
-	$_SESSION["order"]="DESC";
-	$_SESSION["column"]="Year";
+	//session_start();
+	$order="DESC";
+	$column="Year";
+	$sortOption = array ('Highest Price','Lowest Price','Newest','Oldest','Highest Mileage','Lowest Mileage');
+	$validYearMin = 1990;
+	$validYearMax = date("Y");
+	$validPriceMin = 500;
+	$validPriceMax = 10000;
+	$yearErr = $priceErr = "";
+
 ?>
 
 <?php
-	if (isset($_POST["sort"])){
-		if ($_POST["sort"]==="highPrice"){
-			$_SESSION["order"]="DESC";
-			$_SESSION["column"]="Price";
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+		if ($_POST["sort"]==="Highest Price"){
+			$order="DESC";
+			$column="Price";
 		}
-		else if ($_POST["sort"]==="lowPrice"){
-			$_SESSION["order"]="ASC";
-			$_SESSION["column"]="Price";
+		else if ($_POST["sort"]==="Lowest Price"){
+			$order="ASC";
+			$column="Price";
 		}
-		else if ($_POST["sort"]==="new"){
-			$_SESSION["order"]="DESC";
-			$_SESSION["column"]="Year";
+		else if ($_POST["sort"]==="Newest"){
+			$order="DESC";
+			$column="Year";
 		}
-		else if ($_POST["sort"]==="old"){
-			$_SESSION["order"]="ASC";
-			$_SESSION["column"]="Year";
+		else if ($_POST["sort"]==="Oldest"){
+			$order="ASC";
+			$column="Year";
 		}
-		else if ($_POST["sort"]==="highMile"){
-			$_SESSION["order"]="DESC";
-			$_SESSION["column"]="Miles";
+		else if ($_POST["sort"]==="Highest Mileage"){
+			$order="DESC";
+			$column="Miles";
 		}
-		//last option i.e lowMile
+		//last option i.e Lowest Mileage
 		else{
-			$_SESSION["order"]="ASC";
-			$_SESSION["column"]="Miles";
+			$order="ASC";
+			$column="Miles";
+		}
+
+		$yearMin=trim($_POST['yearMin']);
+		$yearMax=trim($_POST['yearMax']);
+		if (empty($yearMin)){
+			$yearErr="Year not provided";
+		}
+		else if (!preg_match("/^[0-9]*$/",$yearMin)){
+			$yearErr="Invalid Year";
+		}
+		else if (empty($yearMax)){
+			$yearErr="Year not provided";
+		}
+		else if (!preg_match("/^[0-9]*$/",$yearMax)){
+			$yearErr="Invalid Year";
+		}
+		else if ($yearMin>$yearMax){
+			$yearErr='Invalid year range';
+		}
+		else{
+			$validYearMin=$yearMin;
+			$validYearMax=$yearMax;
+		}
+
+		$priceMin=trim($_POST['priceMin']);
+		$priceMax=trim($_POST['priceMax']);
+		if (empty($priceMin)){
+			$priceErr="price not provided";
+		}
+		else if (!preg_match("/^[0-9]*$/",$priceMin)){
+			$priceErr="Invalid Price";
+		}
+		else if (empty($priceMax)){
+			$priceErr="Price not provided";
+		}
+		else if (!preg_match("/^[0-9]*$/",$priceMax)){
+			$priceErr="Invalid Price";
+		}
+		else if ($priceMin>$priceMax){
+			$priceErr='Invalid price range';
+		}
+		else{
+			$validPriceMin=$priceMin;
+			$validPriceMax=$priceMax;
 		}
 	}
 ?>
-
-
 
 <html lang="en">
 <head>
@@ -43,8 +93,9 @@
 <link type="text/css" rel="stylesheet" href="carstyles.css"/>
 <style>
 	.car{height:30%; background-color:rgba(255,255,255,0.5);}
-	.carInfo{height:100%; width:50%; background-color:red; float:left; text-align: center; font-size: 20px;}
-	.carImg{height:100%; width:50%; background-color:yellow; float:right; text-align: center;}
+	.carInfo{height:100%; width:50%; float:left; text-align: center; font-size: 20px;}
+	.carImg{height:100%; width:50%; float:right; text-align: center;}
+	.error{color:red;}
 </style>
 <title>Cars homepage</title>
 </head>
@@ -61,62 +112,79 @@
 	</center>
 	
 	<div>
+		
 		<form action="" method="POST">
-			<label for="sort">Sort Option</label>
-			<select name="sort">
-				<option value="highPrice">Highest Price</option>
-				<option value="lowPrice">Lowest Price</option>
-				<option value="new">Newest</option>
-				<option value="old">Oldest</option>
-				<option value="highMile">Highest Mileage</option>
-				<option value="lowMile">Lowest Mileage</option>
-			</select>
-				
-			<input type="submit">
+			<div style="display:inline-block; width:15%; text-align:center;">
+				<label for="sort">Sort Option</label><br>
+				<select name="sort">
+					<?php
+						//list all option in select
+						foreach($sortOption as $option){
+							if ($_POST["sort"]===$option){
+								$selected = "selected";
+							}
+							else{
+								$selected="";
+							}
+							echo "<option value='$option' $selected>$option</option>";
+						}
+					?>
+				</select>
+			</div>
+			<div style="display:inline-block; width:30%; text-align:center;">
+				<label for="year">Year Range:</label><span class="error"><?php echo" *$yearErr"; ?></span><br>
+				<input type="text" value="<?php echo($validYearMin) ?>" name="yearMin"/>
+				<input type="text" value="<?php echo($validYearMax) ?>" name="yearMax"/>
+			</div>
+			<div style="display:inline-block; width:30%; text-align:center;">
+				<label for="year">Price Range:</label><span class="error"><?php echo" *$priceErr"; ?></span><br>
+				<input type="text" value="<?php echo($validPriceMin) ?>" name="priceMin"/>
+				<input type="text" value="<?php echo($validPriceMax) ?>" name="priceMax"/>
+			</div>
+			<div style="display:inline-block; width:15%; text-align:center;">
+				<input type="submit">
+			</div>
 		</form>
 	</div>
 
 		
 <?php
-	//$sql = "SELECT Car.*, image.Image FROM Car LEFT JOIN image ON Car.CarID=image.CarID ORDER BY ".$_SESSION["column"]. " ".$_SESSION["order"].";";
-	//echo $sql;
-	
+
 	include("database_connect.php");
-	//include("database_connect2.php");
-	$sql = "SELECT Car.*, image.Image FROM Car LEFT JOIN image ON Car.CarID=image.CarID ORDER BY ".$_SESSION["column"]. " ".$_SESSION["order"].";";
+	$sql = "SELECT Car.*, image.Image FROM Car LEFT JOIN image ON Car.CarID=image.CarID 
+		WHERE (Year BETWEEN $validYearMin AND $validYearMax) AND (Price BETWEEN $validPriceMin AND $validPriceMax)
+		ORDER BY ".$column. " ".$order.";";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 
 	// output data of each row
-	while($row = $result->fetch_assoc()) {
-		//image path
-    	$imgpath="images/".$row["Image"].".jpg";
+		while($row = $result->fetch_assoc()) {
+			//image path
+	    	$imgpath="images/".$row["Image"].".jpg";
 
-		echo "<div class='car'>";
+			echo "<div class='car'>";
 
-		echo "<div class='carImg'>";
-		echo "<img src='$imgpath' style='height:80%;'>";
-		echo "</div>";
+			echo "<div class='carImg'>";
+			echo "<img src='$imgpath' style='height:100%;'>";
+			echo "</div>";
 
-		echo "<div class='carInfo'>";
-		echo "Make: ".$row['Makes']."<br> Model: ".$row['Models']."<br>Year:".$row["Year"]."<br>Mileage: ".$row["Miles"]."<br>Color: ".$row["Color"]."<br>Price: ".$row["Price"];
-		echo "</div>";
+			echo "<div class='carInfo'>";
+			echo "Make: ".$row['Makes']."<br> Model: ".$row['Models']."<br>Year:".$row["Year"]."<br>Mileage: ".$row["Miles"]."<br>Color: ".$row["Color"]."<br>Price: ".$row["Price"];
+			echo "</div>";
 
-		echo "</div>";
-		echo "<br>";
-    }
-    
-
+			echo "</div>";
+			echo "<br>";
+		}
 	}
-
 	$conn->close();
-
-
+	
+	
 ?>
 
-
+<!-- Testing area-->
 <div>
 <?php 
+
 
 ?>
 </div>
--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 	echo "Make: ".$row['Makes']."<br> Model: ".$row['Models']."<br>Year:".$row["Year"]."<br>Mileage: ".$row["Miles"]."<br>Color: ".$row["Color"]."<br>Price: ".$row["Price"];
-
+	echo date("Y");
+	echo date("Y")+1;
    
 ?>
 
